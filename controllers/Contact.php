19182070542984ea4e7e6608ca72b78e876376b8
RAYCHEV <nikolay.g.raychev@gmail.com>
2017-03-04 12:19:43 +0000

    add Contact controller

---
+++ controllers/Contact.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+
+use MVC\DefaultController;
+
+
+class Contact extends DefaultController
+{
+//    public function about()
+//    {
+//        $this->view->render("user/contact");
+//    }
+    public function show()
+    {
+        $this->view->render("contact/show");
+    }
+
+    public function message()
+    {
+        $input = [];
+        $input['name'] = $this->input->get(0);
+        $this->view->render("contact/sendingMessage", $input);
+    }
+
+}
