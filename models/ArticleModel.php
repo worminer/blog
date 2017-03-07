fbd48e2b63c093f22c2e6e75ce3ff1664b95a7dc
VaniaStoicheva <cecostoi4ev@abv.bg>
2017-03-07 14:00:11 +0200

    Article controller -create article functionaly Article model-create functionaly Article view article functionaly

--- models/ArticleModel.php
+++ models/ArticleModel.php
@@ -3,6 +3,8 @@ namespace Models;
 
 class ArticleModel extends \MVC\Database\PdoMysql
 {
+
+
     private $errorMessage = "";
     /**
      * @param int $author_id
@@ -17,29 +19,21 @@ class ArticleModel extends \MVC\Database\PdoMysql
         if (!isset($author_id) && !isset($title) && !isset($content)) {
             throw new \Exception("ERROR: ArticleModel->create -> all parameters are Mandatory title={$title}, content={$content} <-");
         }
-        //INSERT INTO `articles`( `author_id`, `title`, `content`) VALUES ('3','new article','gergtgrgyrrtge')
-        // приготвяме заявката за записване на артикъла
+
         $result = $this->prepare("INSERT INTO `articles`( `author_id`, `title`, `content`) VALUES (?,?,?)", [$author_id,$title,$content]);
-        // опитваме се да запишем артикъла в базата с данни
+
         $result->execute();
-        // ако е Не успешна заявката то тогава getAffectedRows() ще върне 0 което като булева е false
+
         if  (!$result->getAffectedRows()){
-            // ако е върнало 0/false трябва да покажем грешка
+
             $this->setErrorMessage("Error: article was not saved in DB!");
             return false;
         }else {
-            // ако е успяло да се запише в базата с данни то тогава ние връщаме true
+
             return true;
         }
     }
 
-    public function show(){
-        $result=$this->prepare("SELECT * FROM `articles`");
-        $result->execute();
-
-        return $result->fetchAllAssoc();
-
-    }
 
     private function setErrorMessage(string $message){
         $this->errorMessage = $message;
