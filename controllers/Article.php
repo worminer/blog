c54707cf5768a007aa16ef360a08139a2fc8b21e
VaniaStoicheva <cecostoi4ev@abv.bg>
2017-03-08 17:11:21 +0200

    add validation creatArticle functionaly

--- controllers/Article.php
+++ controllers/Article.php
@@ -15,21 +15,35 @@ class Article extends DefaultController
     {
         $input = $this->input;
 
+        if ($input->post('create') === null){
+            $this->view->redirect("/article/edit");
+        }
+
         if (!$this->auth->isLogged()) {
-            $this->view->redirect("/user/login", "You cant write an article if you are not logged in!");
+            $this->view->redirect("/user/login", "You cant read an post if you are not logged in!");
         }
         $author_id = $this->auth->getCurrentUserId();
 
-        $title = $input->post('title');
+        $title = $input->post('title',"trim");
         $content = $input->post('content');
+
+        $this->validate->setRule('minlength', $title , 5 , "Article title is to short!");
+        $this->validate->setRule('minlength', $content , 10 , "Article content is to short!");
+
+        if ($this->validate->validate() === false) {
+            $errors = $this->validate->getErrors();
+            $this->view->redirect("/article/create",$errors);
+        }
+
 
         $articleModel = new ArticleModel();
 
         if ($articleModel->create($author_id, $title, $content)) {
-            $this->view->redirect("/article/create", "Article Created Successfully.", "success");
+            $this->view->redirect("/article/create", "Post Created Successfully.", "success");
         } else {
             $errorMessage = $articleModel->getErrorMessage();
             $this->view->redirect("/article/create", $errorMessage, "error");
         }
     }
+
 }
