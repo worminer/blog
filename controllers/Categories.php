d7902aca5870b5f759a450f23cb3552e52137d5b
Dimitarsgit <dimitar.s@mail.bg>
2017-03-08 23:10:52 +0200

    Updated: category controller now can create new Categories Updated: manageView.phtml now displays all the Categories

--- controllers/Categories.php
+++ controllers/Categories.php
@@ -8,13 +8,47 @@ class Categories extends DefaultController
 {
     public function manage()
     {
+        if (!$this->auth->isLogged()) {
+            $this->view->redirect("/user/login", "You can not manage categories if you are not logged in!");
+        }
+
         $categoryModel = new CategoriesModel();
         $categories = $categoryModel->getCategories();
-
         $this->view->render("category/manage",["categories"=> $categories]);
     }
 
     public function addPost(){
+
+        if ($this->input->post("category_create") === null){
+            $this->view->redirect("/categories/manage");
+        }
+
+        if (!$this->auth->isLogged()) {
+            $this->view->redirect("/user/login", "You can not add new category if you are not logged in!");
+        }
+
+        $categoryName = $this->input->post("category_name");
+
+        $this->validate->setRule("minlength",$categoryName,3, "Category name length must be more then 3 symbols!");
+
+        if ($this->validate->validate() === false){
+            $error = $this->validate->getErrors();
+            $this->view->redirect("/categories/manage",$error);
+        }
+
+        $categoryModel = new CategoriesModel();
+
+        if ($categoryModel->hasCategory($categoryName)){
+            $this->view->redirect("/categories/manage","This category already exist!");
+        }
+
+        if($categoryModel->addNewCategory($categoryName)){
+            $this->view->redirect("/categories/manage","Category created successfully!","success");
+        }else{
+            $this->view->redirect("/categories/manage","Something goes wrong with the category creation!");
+        }
+
+
 
     }
     public function deletePost(){
--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -22,4 +22,24 @@ class CategoriesModel extends PdoMysql
 
         return $result->fetchAllAssoc();
     }
+
+    public function hasCategory(string $categoryName){
+        try{
+            $result = $this->prepare('SELECT `id` FROM `categories` WHERE name=?', [$categoryName])->execute();
+        }catch (\Exception $exception){
+            return false;
+        }
+        return (bool) $result->fetchRllAssoc();
+    }
+
+    public function addNewCategory(string $categoryName):bool {
+        try{
+            $result = $this->prepare('INSERT INTO `categories` (`name`) VALUES (?)', [$categoryName])->execute();
+        }catch (\Exception $exception){
+            return false;
+        }
+
+
+        return (bool) $result->getAffectedRows();
+    }
 }
